e73e4a9075c5e2f486b8428f3b8182bcc0ead534
Matias Viecho <mpviecho@gmail.com>
2023-08-01 00:39:53 -0300

    feat(grid): improved checks on where to render a 3 column grid or 4 column grid

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -7,6 +7,9 @@ export interface iGridItem {
   id: number;
 }
 
+const primeNumbers = [
+  3, 7, 9, 11, 13, 15, 21, 23, 27, 17, 19, 31, 37, 41, 43, 47, 71, 73, 79, 97,
+];
 export const Grid: React.FC<{
   items: iGridItem[];
   handleDelete: (a: number) => void;
@@ -15,22 +18,18 @@ export const Grid: React.FC<{
   const length = items.length;
 
   const getDivider = () => {
-    if (
-      length % 3 === 0 ||
-      length === 2 ||
-      length === 5 ||
-      length === 7 ||
-      length === 11 ||
-      length === 13 ||
-      length === 17
-    ) {
-      return 3;
-    } else if (length % 4 === 0 || (length % 2 === 0 && length !== 2)) {
+    if (length % 4 === 0 || (length % 2 === 0 && length !== 2)) {
+      console.log('get ');
       return 4;
+    } else if (length % 3 === 0 || length % 5 === 0) {
+      return 3;
+    } else if (primeNumbers.includes(length)) {
+      return 3;
     } else {
       return -1;
     }
   };
+
   return (
     <section
       style={
@@ -41,6 +40,10 @@ export const Grid: React.FC<{
       }
       className={style['container']}
     >
+      <p className="text-lg uppercase text-white text-semibold spaced">
+        Ahora mismo hay {length} {length > 1 ? 'items' : 'item'}
+      </p>
+
       <ul className={`${style['grid-container']}`}>
         {items.map((item, index) => {
           return (
