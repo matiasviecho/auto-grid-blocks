79ae10e2aabe4a219b86a8de40ddf953bf8f00b0
Matias Viecho <mpviecho@gmail.com>
2023-08-01 00:39:30 -0300

    feat(app): add functionalities to add an item, remove an item, save the current array and restore the array to saved version

--- src/App.tsx
+++ src/App.tsx
@@ -16,25 +16,75 @@ function App() {
     { title: 'Item 6', color: '#9e9e9e', id: 10 },
   ]);
   const itemsRef = useRef<iGridItem[] | null>();
+  const idRef = useRef<number>(11);
   useEffect(() => {
     itemsRef.current = items;
   }, []);
+
+  const saveArray = () => {
+    itemsRef.current = items;
+  };
 
   const handleDelete = (id: number) => {
     setItems((prevItems) => prevItems.filter((item, _index) => item.id !== id));
   };
 
-  const addItem = () => {
+  const addItem = (e: React.FormEvent) => {
+    const getColor = () => {
+      const number = Math.random();
+      if (number < 0.25) {
+        return '#241468';
+      } else if (number < 0.5) {
+        return '#9F0D7F';
+      } else if (number < 0.75) {
+        return '#EA1179';
+      } else {
+        return '#F79BD3';
+      }
+    };
+
+    e.preventDefault();
     setItems((prevItems) => [
       ...prevItems,
-      { title: 'New Item', color: 'green', id: Date.now() },
+      {
+        title: titleRef.current || '',
+        color: getColor(),
+        id: idRef.current,
+      },
     ]);
+    idRef.current = idRef.current + 1;
+  };
+  const titleRef = useRef<string>();
+
+  const restoreArray = () => {
+    if (typeof itemsRef.current !== 'undefined' && itemsRef.current) {
+      setItems(itemsRef.current);
+    }
   };
   return (
     <main
-      className={`content flex flex-col m-auto items-center justify-center`}
+      className={`content flex flex-col m-auto items-center justify-center gap-16`}
     >
-      <div></div>
+      <div className="flex gap-8">
+        <button onClick={() => saveArray()}>Save Grid</button>
+        <form
+          onSubmit={(e) => addItem(e)}
+          className="flex gap-16"
+        >
+          <input
+            type="text"
+            name="title"
+            id="title"
+            maxLength={20}
+            onChange={(e) => {
+              titleRef.current = e.target.value;
+            }}
+            placeholder="Titulo"
+          />
+          <button type="submit">Guardar Item</button>
+        </form>
+        <button onClick={() => restoreArray()}>Restore Grid</button>
+      </div>
       <Grid
         items={items}
         handleDelete={handleDelete}
